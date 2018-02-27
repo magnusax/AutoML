952dbbb76ed54209f6597d15192bc2d9d5f9bd00
Axelsson <magnus.axelsson@finn.no>
2018-02-27 13:46:18 +0100

    Modified ensembler.py

--- gazer-learn/ensembler.py
+++ gazer-learn/ensembler.py
@@ -1,8 +1,13 @@
-import numpy as np
+import os
+import sys
 import warnings
 from copy import deepcopy
 
+import numpy as np
 from scipy.stats import uniform
+from sklearn.externals import joblib
+
+
 from sampling import Loguniform
 from library import library_config
 from gazer import GazerMetaLearner
@@ -24,20 +29,47 @@ def ensemble_builder(X, names=None):
     
     return {name:_generate(name, grid) for name, grid in _lib}
 
-def fit_ensemble(ens, X, y):
+def fit_ensemble(ens, X, y, save_dir=None):
     """ Fit an ensemble of algorithms 
     Input:
-        ens: dictionary of (name, learner) tuples. The learner must have a fit method.
-        X: 2D matrix of shape (n_samples, n_columns)
-        y: Label vector of shape (n_samples,)
+        ens (dict): dictionary of (name, learner) tuples. 
+            The learner must have a fit method.
+        X (2D array-like or matrix-like): 2D matrix of shape (n_samples, n_columns)
+        y (array-like): Label vector of shape (n_samples,)
+        save_dir (string): directory to pickle fitted algorithms
     Output:
-        List of fitted learners
+        List of fitted learners. If save_dir is a valid directory it will contain the pickled
+        versions of all fitted classifiers
     """
     if not isinstance(ens, dict):
-        raise TypeError(__name__+".fit_ensemble: expects 'ens' to be of type dictionary")
-        
-    return [clf.fit(X,y) for _, clfs in ens.items() for clf in clfs]
-           
+        raise TypeError(__name__+".fit_ensemble: expect 'ens' to be a dictionary.")
+    
+    if save_dir is not None:
+        if save_dir[-1]!="/":
+            save_dir += "/"        
+        if not os.path.isdir(save_dir):
+            try:
+                os.makedirs(save_dir)
+            except:
+                raise ValueError("Could not create '%s'." % save_dir)
+    
+    fitted = []
+    for name, clfs in ens.items():
+        total_fits = len(clfs)
+        for i, clf in enumerate(clfs):
+            fitted.append(clf.fit(X,y))
+            
+            if save_dir is not None:
+                model_name = 'model_%s_%s.pkl' % (name, (i+1))
+                try:
+                    joblib.dump(clf, save_dir+model_name)
+                except:
+                    warnings.warn("Could not pickle '%s'." % model_name)
+            
+            show_progress((i+1)*(100/total_fits))
+        print("Fitted all versions of '%s' algorithm." % name)
+    return fitted
+
 def _generate(estimator_name, estimator_params):    
     """ Here we generate estimators to later fit. """
     
@@ -96,3 +128,10 @@ def _generate_grid(grid):
                 return logs.range()
     else:
         raise ValueError(__name__+"._generate_grid: 'method' should be (take, sample)")
+
+        
+def show_progress(p):
+    sys.stdout.write('\r')
+    sys.stdout.write("[%-100s] %d%%" % ('=' * int(p), p))
+    sys.stdout.flush()
+    return
