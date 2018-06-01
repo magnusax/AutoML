5ca2e23300c2afb810aa336d01f7c93220192ca1
Magnus Axelsson <magnus.axelsson@inmeta.no>
2018-06-01 13:38:42 +0200

    mod

--- gazer/classifiers/neural_network.py
+++ gazer/classifiers/neural_network.py
@@ -1,27 +1,36 @@
+from __future__ import division
+from __future__ import print_function
+
 from ..base import BaseClassifier
+from ..externals.clr_callback import CyclicLR
 
 import os
 import warnings
+import collections
 import numpy as np
 import pandas as pd
 
 import keras
+import keras.backend as K
 from keras.models import Sequential
-from keras.callbacks import (LearningRateScheduler, 
-                             ReduceLROnPlateau, 
-                             ModelCheckpoint)
-from keras.layers import (Activation, 
-                          BatchNormalization, 
-                          Dense, Dropout)
 
-from ..externals.clr_callback import CyclicLR
+from keras.callbacks import (
+        EarlyStopping,
+        LearningRateScheduler, 
+        ReduceLROnPlateau, 
+        ModelCheckpoint)
+
+from keras.layers import (
+        Activation, 
+        BatchNormalization, 
+        Dense, Dropout)
 
 
 class MetaNeuralNetworkClassifier(BaseClassifier):
     
-    def __init__(self, epochs=50, batch_size=16, optimizer='adam', learning_rate=1e-4, 
+    def __init__(self, epochs=50, batch_size=32, optimizer='adam', learning_rate=1e-3, 
                  n_hidden=2, p=0.1, dropout=True, batch_norm=False, decay_units=False, input_units=250):
-        
+    
         """
         Parameters:
         ------------
@@ -33,10 +42,12 @@ class MetaNeuralNetworkClassifier(BaseClassifier):
                 Batch size used in the fit method.
                 
             optimizer : string or keras callable. Default: str, 'adam'
-                The optimizer to use when compiling.
+                The optimizer to use when compiling. Note that 'learning_rate'
+                overrides any learning rate that was set in the callable, so
+                make sure the values match.
                 
-            learning_rate : float, default: 1e-4
-                Specify learning rate if applicable. 
+            learning_rate : float, default: 1e-3
+                Specify learning rate to use in the optimizer. 
                 
             n_hidden : integer, default: 2
                 Specify the number of hidden dense layers.
@@ -71,8 +82,9 @@ class MetaNeuralNetworkClassifier(BaseClassifier):
         self.network = {}
         self.network['epochs'] = epochs
         self.network['batch_size'] = batch_size
-        self.network['optimizer'] = optimizer
         self.network['lr'] = learning_rate
+        self.network['optimizer'] = optimizer
+        self.network['history'] = None
         
         # Specify architecture
         self.network['n_hidden'] = n_hidden
@@ -110,7 +122,7 @@ class MetaNeuralNetworkClassifier(BaseClassifier):
                 'does_regression': False, 
                 'predict_probas': True}
     
-    
+            
     def set_architecture(self, input_shape, output_shape):
         """
         Called prior to building and compiling the keras model.
@@ -134,9 +146,7 @@ class MetaNeuralNetworkClassifier(BaseClassifier):
         in the number of hidden layers, hidden units, regularization, activation, 
         and learning rates.
         
-        """ 
-        assert self.ready==True
-        
+        """         
         units = self.network['units']
         activations = self.network['activation']
         add_batchnorm = self.network['batchnorm']
@@ -156,9 +166,13 @@ class MetaNeuralNetworkClassifier(BaseClassifier):
                 model.add(Dropout(p))
             
         model.add(Dense(units=self.output_shape, activation='softmax'))                
-        model.compile(loss='categorical_crossentropy',
-                      optimizer=self.network['optimizer'],
-                      metrics=['accuracy'])
+        model.compile(
+            loss='categorical_crossentropy',
+            optimizer=self.network['optimizer'],
+            metrics=['accuracy'])
+        # Use the backend to set the base learning rate
+        K.set_value(model.optimizer.lr, self.network['lr'])
+        
         return model
     
     
@@ -187,17 +201,20 @@ class MetaNeuralNetworkClassifier(BaseClassifier):
         Implement callbacks.
         """
         # Reduce learning rate on plateau
-        reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.5, patience=5, min_lr=1e-6)
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10, min_lr=1e-5)
+        
+        # Early stopping
+        early_stop = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=50)
         
         # Checkpointing
         filepath = os.path.join(os.getcwd(), "tmp")
         if not os.path.exists(filepath):
             os.makedirs(filepath)
-        filepath = os.path.join(filepath, "weights.{epoch:02d}_{loss:.2f}.hdf5")       
-        
+        filepath = os.path.join(filepath, "weights.{epoch:02d}_{loss:.2f}.hdf5")              
         checkpointing = ModelCheckpoint(filepath, monitor='val_loss', save_best_only=False, 
                                         save_weights_only=True, period=10)           
-        return[reduce_lr, checkpointing]
+        
+        return[reduce_lr, early_stop]
     
     
     def lr_finder(self, X, y, monitor='acc', step_size=2000):
@@ -209,7 +226,8 @@ class MetaNeuralNetworkClassifier(BaseClassifier):
             raise ValueError("monitor should be either 'acc' or 'loss'.")
             
         if step_size is None:
-            step_size = int(np.ceil(X.shape[0]/self.network['batch_size']))
+            # Authors recommend step_size = 2-8 * iterations_per_epoch
+            step_size = 8 * int(np.ceil(X.shape[0]/self.network['batch_size']))
         clr = CyclicLR(base_lr=1e-6, max_lr=10.0, 
                        step_size=step_size, mode='triangular', lr_find=True)
         
@@ -218,7 +236,7 @@ class MetaNeuralNetworkClassifier(BaseClassifier):
         callbacks_old, epoch_old = \
             (self.network['callbacks'], self.network['epochs'])
         
-        self.network['callbacks'], self.network['epochs'] = [clr], 5
+        self.network['callbacks'], self.network['epochs'] = [clr], 10
         self.fit(X, y)
         
         clr = self.network['callbacks'][0]        
@@ -229,11 +247,12 @@ class MetaNeuralNetworkClassifier(BaseClassifier):
         self.network['callbacks'], self.network['epochs'] = \
             (callbacks_old, epoch_old)
         self.estimator = None
+        self.ready = False
         
         return (xlr, ylr)
     
     
-    def fit(self, X, y, ):
+    def fit(self, X, y, **kwargs):
         
         # We keep a local copy of the labels 
         # to avoid modifying the original input data
@@ -247,13 +266,20 @@ class MetaNeuralNetworkClassifier(BaseClassifier):
                    \nWill attempt to apply one-hot encoding before sending to `fit` method.""")
             y_ = keras.utils.to_categorical(y_)
         
-        # Freeze architecture
-        self.set_architecture(X.shape[1], y_.shape[1])
-        # Define estimator
-        self.estimator = self._get_clf()
-        # Fit estimator
-        self.estimator.fit(X, y_, 
-                           batch_size=self.network['batch_size'], 
-                           epochs=self.network['epochs'],
-                           callbacks=self.network['callbacks'])     
+        # This will only happen once as `set_architecture` modifies
+        # this variable. Successive calls to `fit` will not cause
+        # a recompilation of the model (i.e. you don't need to start from scratch!)
+        if self.ready == False:
+            # Freeze architecture
+            self.set_architecture(X.shape[1], y_.shape[1])
+            
+            # Define estimator and compile keras model
+            self.estimator = self._get_clf()
         
+        self.network['history'] = \
+            self.estimator.fit(
+                X, y_, 
+                batch_size = self.network['batch_size'], 
+                epochs = self.network['epochs'],
+                callbacks = self.network['callbacks'],
+                **kwargs)     
